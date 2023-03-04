aa0607870fd98377a2aa39906ce8f5de6a329e2a
Stefan Bearson <trancedude79@hotmail.com>
2023-03-04 11:10:00 +0100

    Add Layoutservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { NewHeaderComponent } from './layout/new-header/new-header.component';
+
+import { LayoutService } from './services/layout.service'
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, ContentLayoutComponent
+    AppComponent, HeaderComponent, ContentLayoutComponent, NewHeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { LayoutService } from 'src/app/services/layout.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnChanges {
+  leftOpen$: Observable<boolean> = new Observable<boolean>
+  rightOpen$: Observable<boolean> = new Observable<boolean>
 
-  constructor() { }
+  constructor(private layoutService: LayoutService) { 
+  }
 
   ngOnInit(): void {
+    this.checkSidebars();
+  }
+  ngOnChanges(){
+    this.checkSidebars();
+  }
+  
+  checkSidebars(): void {
+    // this.layoutService.leftSidebarIsOpen().subscribe(open => this.leftOpen$ = Observable<Boolean>(open.valueOf));
+    // this.layoutService.rightSidebarIsOpen().subscribe(open => this.rightOpen$.subscribe = open);
+    
+
   }
 
 }
