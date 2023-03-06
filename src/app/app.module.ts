711654fe0d2a4d85dc66589e4a6fbf82649d046c
Stefan Bearson <trancedude79@hotmail.com>
2023-03-06 12:37:20 +0100

    Add menu and option component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { AppComponent } from './app.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NewHeaderComponent } from './layout/new-header/new-header.component';
-
-import { LayoutService } from './services/layout.service'
+import { OptionsComponent } from './layout/options/options.component'
+import { MenuComponent } from './layout/menu/menu.component'
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, ContentLayoutComponent, NewHeaderComponent
+    AppComponent, HeaderComponent, ContentLayoutComponent, NewHeaderComponent, OptionsComponent, MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/options/options.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import themes from 'devextreme/ui/themes'
+@Component({
+  selector: 'app-options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.css']
+})
+export class OptionsComponent implements OnInit {
+  darkmode = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggleDarkmode() {
+    if(!this.darkmode){
+      themes.current("generic.dark")
+      document.documentElement.classList.add('dark')
+      this.darkmode = true
+    }else{
+      themes.current("generic.light")
+      document.documentElement.classList.remove('dark')
+      this.darkmode = false
+    }
+  }
+}
